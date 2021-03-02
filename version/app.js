9aef679272c54a6424f67f3ea8cb1450157833e9
farhan <farhantheaven@gmail.com>
2021-03-02 02:41:06 -0800

    modifying branch

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'Farhan is testing this app' + '\n';
+    response = 'Farro is Farro' + '\n';
 
     //send the response to the client
     res.send(response);

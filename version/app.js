c06dec705a98e2a16205607adb2d19808f56102e
farhan <farhantheaven@gmail.com>
2021-03-02 03:04:48 -0800

    Start building when pushing branch to github

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'Farro is Farro' + '\n';
+    response = 'Checking build on github push' + '\n';
 
     //send the response to the client
     res.send(response);
